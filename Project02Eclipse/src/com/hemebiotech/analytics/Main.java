ee7c37a30b0aec8359ff3010dff3d6ed6da6a28c
sylvainsbr <sylvainsouber@gmail.com>
2020-01-17 00:58:51 +0100

    Implementation execute dans le main et Imports

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,5 +1,22 @@
 package com.hemebiotech.analytics;
 
+import count.Count;
+import count.ICount;
+import read.ISymptomReader;
+import read.ReadSymptomDataFromFile;
+import write.IWriteResult;
+import write.WriteResult;
+
 public class Main {
 
+	public static void main(String[] args) throws Exception {
+
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ICount counter = new Count();
+        IWriteResult writer = new WriteResult("result.out");
+
+        new AnalyticsCounter(
+                reader,counter,writer
+        ).execute();
+    }
 }
