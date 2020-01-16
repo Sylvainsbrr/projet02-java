2ad7233537825451a403815d6a6e71a12b945ccd
sylvainsbr <sylvainsouber@gmail.com>
2020-01-16 18:12:40 +0100

    Hotfix2 AnalyticsCounter With Hashmap

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,61 @@
 package com.hemebiotech.analytics;
 
+import java.util.*;
+import java.util.Map.Entry;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
+
+	static Map<String, Integer> frequency = new HashMap<>();
+
+	static BufferedWriter writer;
+
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		
-		
-		while (line != null) {
-			
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+		// File Reader
+		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		//File writer
+		writer = new BufferedWriter(new FileWriter("result.out", true));
+
+		String line;
+
+		String symptoms;
+
+		// read lines until reaching the end of the file
+		while ((line = reader.readLine()) != null) {
+
+			symptoms = line;
+
+			if (line.length() != 0) {
+
+				if (frequency.containsKey(symptoms)) {
+					frequency.put(symptoms, frequency.get(symptoms) + 1);// Increment 1 if a key match a symptom
+				} else {
+					frequency.put(symptoms, 1);// If the key match only one time set value to 1
+				}
+
 			}
 
-			line = reader.readLine();	// get another symptom
+		}
+		//Writting the output on the result.out file
+		for (Entry<String, Integer> count : frequency.entrySet()) {
+			System.out.println(count.getKey() + "=" + count.getValue());//preview result.out file
+			writer.write(count.getKey() + "=" + count.getValue());
+			writer.newLine();
+			writer.flush();
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		// Closing Resources
+		reader.close();
+
 	}
+
+
+	
 }
