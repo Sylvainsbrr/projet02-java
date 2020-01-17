f91a7504eb33ed18c62214bb69294b4682331c41
sylvainsbr <sylvainsouber@gmail.com>
2020-01-17 01:06:56 +0100

    Modif  Import AnalyticCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,16 +1,11 @@
 package com.hemebiotech.analytics;
 
 import java.util.*;
-import java.util.Map.Entry;
-
 import count.ICount;
 import read.ISymptomReader;
 import write.IWriteResult;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 
 
 public class AnalyticsCounter {
@@ -26,16 +21,13 @@ public class AnalyticsCounter {
     }
 	
 	public void execute() throws Exception {
-        //2eme étape: On lit le fichier symptoms.txt
+        //1eme étape: On lit le fichier symptoms.txt
         List<String> allSymptoms = reader.getSymptoms();
 
-        //3eme étape: On utilise la classe CountSymptom cette fois ci pour compter les symptoms.
+        //2eme étape: On utilise la classe CountSymptom cette fois ci pour compter les symptoms.
         Map<String, Integer> symptomsCounter = counter.count(allSymptoms);
-
-        //4eme étape: On range dans l'ordre alphabétique les symptoms.
-        List<String> symptoms = sorter.sort(symptomsCounter.keySet());
-
-        //5eme étape: On écrit le fichier result.out
+        Set<String> symptoms = symptomsCounter.keySet();
+        //3eme étape: On écrit le fichier result.out
         for (String symptom: symptoms){
             writer.write(symptom, symptomsCounter.get(symptom));
         }
